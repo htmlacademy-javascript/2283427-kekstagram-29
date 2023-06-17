42397b85182ae08d8e989442ce18514d35b1face
Николай <nik-zhitelev@yandex.ru>
2023-06-17 20:27:15 +0300

    Добавляет третью функцию

--- js/functions.js
+++ js/functions.js
@@ -24,3 +24,19 @@ const palindromeString = function (checkedString) {
 };
 
 palindromeString('Лёша на полке клопа нашёл ');
+
+// Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа
+
+function getNumbersFromString(string) {
+  string = typeof string === 'number' ? string.toString() : string;
+  let result = '';
+  for (let i = 0; i <= string.length - 1; i++) {
+    const number = parseInt(string[i], 10);
+    if (Number.isNaN(number) === false) {
+      result += number;
+    }
+  }
+  return result.length > 0 ? Number(result) : NaN;
+}
+
+getNumbersFromString('dfgsdfg 324q2asdf ');
