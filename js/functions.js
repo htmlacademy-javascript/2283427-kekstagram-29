7838752445cdd360a7fb38b6b49088a33ae9aa6f
Николай <nik-zhitelev@yandex.ru>
2023-06-21 22:49:22 +0300

    Решает дополнительную задачу 5.16.

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-//Функция для проверки длины строки
+// Функция для проверки длины строки
 
 const lengthString = function (checkedString, maxLength) {
   if (checkedString.length <= maxLength) {
@@ -25,7 +25,12 @@ const palindromeString = function (checkedString) {
 
 palindromeString('Лёша на полке клопа нашёл ');
 
-// Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа
+/*
+
+Функция, которая принимает строку, извлекает содержащиеся в ней цифры
+от 0 до 9 и возвращает их в виде целого положительного числа
+
+*/
 
 function getNumbersFromString(string) {
   string = typeof string === 'number' ? string.toString() : string;
@@ -40,3 +45,37 @@ function getNumbersFromString(string) {
 }
 
 getNumbersFromString('dfgsdfg 324q2asdf ');
+
+/*
+
+Функция, которая принимает время начала и конца рабочего дня,
+а также время старта и продолжительность встречи в минутах и возвращает true,
+если встреча не выходит за рамки рабочего дня, и false, если выходит.
+
+*/
+
+const calcMinInHour = function (timeInHour) {
+  const timeParts = timeInHour.split(':');
+  return Number(timeParts[0]) * 60 + Number(timeParts[1]);
+};
+
+const getWorkTime = function (from, to, start, durationMin) {
+  if ((calcMinInHour(start) + durationMin) <= calcMinInHour(to) && calcMinInHour(from) <= calcMinInHour(start)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+getWorkTime();
+
+/*
+
+Проверка
+console.log(getWorkTime('08:00', '17:30', '14:00', 90)); // true
+console.log(getWorkTime('8:0', '10:0', '8:0', 120));     // true
+console.log(getWorkTime('08:00', '14:30', '14:00', 90)); // false
+console.log(getWorkTime('14:00', '17:30', '08:0', 90));  // false
+console.log(getWorkTime('8:00', '17:30', '08:00', 900)); // false
+
+*/
